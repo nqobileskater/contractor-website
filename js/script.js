24d50f6b5c0ffb680b2d392106aa3bf9c1d74cd6
Nqobile Shinga <121100819+nqobileskater@users.noreply.github.com>
2025-02-15 13:42:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+// Hero Section Slideshow
+const hero = document.querySelector('.hero');
+const images = [
+  'url("images/hero1.jpg")',
+  'url("images/hero2.jpg")',
+  'url("images/hero3.jpg")'
+];
+let currentImageIndex = 0;
 
+function changeBackground() {
+  hero.style.backgroundImage = images[currentImageIndex];
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+// Change background every 5 seconds
+setInterval(changeBackground, 5000);
+
+// Set the initial background
+changeBackground();
